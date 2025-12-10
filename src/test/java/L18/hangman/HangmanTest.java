f08382ca8b4ee6333a2bc2b7118d412dfadf38de
SozialNomad <dogan1furkan1@gmail.com>
2025-12-10 23:22:45 +0100

    .

---
+++ src/test/java/L18/hangman/HangmanTest.java
@@ -0,0 +1,23 @@
+package L18.hangman;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HangmanTest {
+    @Test
+    public void testeTCPServer(){
+        final int PORT = 5000;
+        try(ServerSocket ss = new ServerSocket(PORT);
+            Socket conn = ss.accept();
+            //OutputStream os = new OutputStream();
+        ){
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
